7d68bac5dff4ca6581c440bd16655506a95ca2ca
Goran Ehrsson <goran@technipelago.se>
2013-06-22 16:24:16 +0200

    Version 1.1.0

--- CrmTaskUiGrailsPlugin.groovy
+++ CrmTaskUiGrailsPlugin.groovy
@@ -22,7 +22,7 @@ class CrmTaskUiGrailsPlugin {
     // Dependency group
     def groupId = "grails.crm"
     // the plugin version
-    def version = "1.1-SNAPSHOT"
+    def version = "1.1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
