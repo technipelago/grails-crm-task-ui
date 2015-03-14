5917bcb0f92c4f4216469be5c395462729ba92a6
Goran Ehrsson <goran@technipelago.se>
2015-03-14 19:29:03 +0100

    Upgraded dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,7 @@ grails.project.dependency.resolution = {
         test(":codenarc:0.21") { export = false }
         test(":code-coverage:1.2.7") { export = false }
 
-        compile ":crm-core:2.0.1"
+        compile ":crm-core:2.0.2"
         compile ":crm-task:2.0.0"
         compile ":crm-contact:2.0.2"
         compile ":crm-ui-bootstrap:2.0.0"
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
 
         compile ":decorator:1.1"
         compile ":user-tag:0.6"
-        compile ":selection:0.9.7"
+        compile ":selection:0.9.8"
         compile ":selection-repository:0.9.3"
     }
 }
