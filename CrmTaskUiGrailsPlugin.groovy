407b1c7605f8b1f7bc367ec65066b4dbef661fa6
Goran Ehrsson <goran@technipelago.se>
2015-03-14 22:47:46 +0100

    Version 2.4.0

--- CrmTaskUiGrailsPlugin.groovy
+++ CrmTaskUiGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmTaskUiGrailsPlugin {
     def groupId = ""
-    def version = "2.4.0-SNAPSHOT"
+    def version = "2.4.0"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTask']
