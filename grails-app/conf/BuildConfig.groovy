d00d29594bf746f1442dd14a303f6791f6b2e2e0
Goran Ehrsson <goran@technipelago.se>
2015-10-18 11:32:15 +0200

    Upgraded user-tag to 1.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,9 +43,9 @@ grails.project.dependency.resolution = {
         compile ":selection-repository:0.9.3"
         compile ":recent-domain:0.6.0"
         compile ":decorator:1.1"
-        compile ":user-tag:0.6"
+        compile ":user-tag:1.0"
 
-        compile ":crm-task:2.4.2"
+        compile ":crm-task:2.4.3-SNAPSHOT"
         compile ":crm-contact:2.4.1"
         compile ":crm-feature:2.4.1"
         compile ":crm-ui-bootstrap:2.4.0"
