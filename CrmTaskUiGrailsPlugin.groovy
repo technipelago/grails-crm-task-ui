c8fe28df5a0ccdd3e584110dc1234f2d1de033d7
Goran Ehrsson <goran@technipelago.se>
2016-09-24 23:28:50 +0200

    Next snapshot version

--- CrmTaskUiGrailsPlugin.groovy
+++ CrmTaskUiGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class CrmTaskUiGrailsPlugin {
     def groupId = ""
-    def version = "2.4.3"
+    def version = "2.4.4-SNAPSHOT"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTask']
